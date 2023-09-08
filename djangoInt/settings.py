e7e153a227e16dab0a8dff8ed41e6d6b7d5bde89
AndreyBoyarchuk <andreyboyarchuk@gmail.com>
2023-09-07 23:49:56 -0600

    requirments

--- djangoInt/settings.py
+++ djangoInt/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+
+from dotenv import load_dotenv
+load_dotenv()
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
